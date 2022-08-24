144f77b83392ef490d9a2ad5deafdb4d52b35cc1
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-24 15:01:44 +1200

    testing pipeline setup

--- src/stringFilter.ts
+++ src/stringFilter.ts
@@ -1,7 +1,7 @@
 const regexReplace = /[^a-zA-Z0-9]/g;
 
 const stringFilter = (input:string) => {
-const newString = input.replace(regexReplace, "");
+const newString = input.replace(regexReplace, " ");
 return newString
 };
 
